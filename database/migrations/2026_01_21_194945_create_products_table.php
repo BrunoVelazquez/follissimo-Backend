2a44fe6270ab58077adff52814f5cfef4ea061ac
Bruno Velazquez <42704038+BrunoVelazquez@users.noreply.github.com>
2026-01-21 18:04:30 -0300

    Migrations

---
+++ database/migrations/2026_01_21_194945_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('price');
+            $table->string('color');
+            $table->foreignId('category_id')
+                ->constrained('categories')
+                ->onDelete('restrict');
+            $table->foreignId('size_id')
+                ->constrained('sizes')
+                ->onDelete('restrict');
+            $table->string('description');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
